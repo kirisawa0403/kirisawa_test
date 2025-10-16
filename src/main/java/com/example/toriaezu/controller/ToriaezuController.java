a068e4f89a1df60cb9daa66a8277aa0f80f75ca3
kannn <kannn@192.168.1.4>
2025-10-16 20:59:25 +0900

    画面表示できるように修正

--- src/main/java/com/example/toriaezu/controller/ToriaezuController.java
+++ src/main/java/com/example/toriaezu/controller/ToriaezuController.java
@@ -2,12 +2,10 @@ package com.example.toriaezu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class ToriaezuController {
-	@GetMapping
+	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
