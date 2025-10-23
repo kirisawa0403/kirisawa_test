402595fd864ccd854c39f4b09dd28aa6e53c42d4
kannn <kannn@192.168.1.4>
2025-10-23 19:41:37 +0900

    10/23

--- src/main/java/com/example/toriaezu/controller/ToriaezuController.java
+++ src/main/java/com/example/toriaezu/controller/ToriaezuController.java
@@ -1,12 +1,14 @@
 package com.example.toriaezu.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.example.toriaezu.model.TestDTO; // ← TestDTO のパスに合わせて変更してください
+import com.example.toriaezu.model.TestDTO;
 
 @Controller
 public class ToriaezuController {
@@ -19,11 +21,14 @@ public class ToriaezuController {
         return "index";
     }
 
+
     // フォーム送信（POST）
     @PostMapping("/input")
     public String inputSample(@ModelAttribute("inputTest") TestDTO inputData, Model model) {
         model.addAttribute("test", "テスト表示");
         model.addAttribute("inputData", inputData.getName());
+        model.addAttribute("events", List.of(inputData));
         return "index";
     }
+
 }
--- src/main/java/com/example/toriaezu/model/TestDTO.java
+++ src/main/java/com/example/toriaezu/model/TestDTO.java
@@ -1,13 +1,45 @@
 package com.example.toriaezu.model;
 
-public class TestDTO {
-    private String name;
+import java.time.LocalDate;
 
+import lombok.Data;
+
+@Data
+public class TestDTO {
+    private String title;
+    private LocalDate date; // java.time.LocalDate
+    private String name;
+    private String detail;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+    
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
         this.name = name;
+    }
+    
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
     }
 }
